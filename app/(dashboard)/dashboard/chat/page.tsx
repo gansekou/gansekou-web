7eae9a5c86722f3d052b26a8d61067d1fb1d05e6
gansekou <dev@gansekou.com>
2026-07-21 05:23:25 +0200

    Create page.tsx

---
+++ app/(dashboard)/dashboard/chat/page.tsx
@@ -0,0 +1,22 @@
+import { KoumaChat } from "@/components/chat/KoumaChat";
+
+
+export default function ChatPage(){
+
+
+return (
+
+<div
+className="
+h-[calc(100vh-80px)]
+"
+>
+
+<KoumaChat/>
+
+</div>
+
+);
+
+
+}
