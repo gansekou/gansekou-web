42880fd3f7e0aa2d163bab73c685cd89abb50ddf
gansekou <dev@gansekou.com>
2026-08-23 02:26:54 +0200

    Update AnalyticsProvider.tsx

--- components/analytics/AnalyticsProvider.tsx
+++ components/analytics/AnalyticsProvider.tsx
@@ -5,7 +5,9 @@ import { analytics } from "@/lib/analytics";
 
 export function AnalyticsProvider() {
   useEffect(() => {
-    analytics.track("gansekou_test", {
+    console.log("[GA4] AnalyticsProvider chargé");
+
+    void analytics.track("gansekou_test", {
       platform: "web",
     });
   }, []);
